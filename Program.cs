f2e09570fcd523ec74e871d3182aae9c167be0d4
Kamodov21 <arturk-1708@mail.ru>
2025-02-26 10:27:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class HexTable
+{
+    public static void Main(string[] args)
+    {
+        int tableSize = 16; // Размер таблицы (до F в шестнадцатеричной системе)
+
+        // Печать таблицы умножения
+        Console.WriteLine("Таблица умножения (шестнадцатеричная)");
+        Console.Write("   "); // Отступ для заголовка
+        for (int i = 0; i < tableSize; i++)
+        {
+            Console.Write("{0,3} ", i.ToString("X")); // Заголовок столбцов (шестнадцатеричные числа)
+        }
+        Console.WriteLine();
+        Console.WriteLine("---" + new string('-', tableSize * 4)); // Разделитель
+
+        for (int i = 0; i < tableSize; i++)
+        {
+            Console.Write("{0,2}| ", i.ToString("X")); // Заголовок строк (шестнадцатеричные числа)
+
+            for (int j = 0; j < tableSize; j++)
+            {
+                Console.Write("{0,3} ", (i * j).ToString("X")); // Результат умножения в шестнадцатеричной системе
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // Печать таблицы сложения
+        Console.WriteLine("Таблица сложения (шестнадцатеричная)");
+        Console.Write("   "); // Отступ для заголовка
+        for (int i = 0; i < tableSize; i++)
+        {
+            Console.Write("{0,3} ", i.ToString("X")); // Заголовок столбцов (шестнадцатеричные числа)
+        }
+        Console.WriteLine();
+        Console.WriteLine("---" + new string('-', tableSize * 4)); // Разделитель
+
+        for (int i = 0; i < tableSize; i++)
+        {
+            Console.Write("{0,2}| ", i.ToString("X")); // Заголовок строк (шестнадцатеричные числа)
+
+            for (int j = 0; j < tableSize; j++)
+            {
+                Console.Write("{0,3} ", (i + j).ToString("X")); // Результат сложения в шестнадцатеричной системе
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey(); 
+    }
+}
