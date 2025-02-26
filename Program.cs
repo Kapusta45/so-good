4ce43c904f4e55b9089be8cf69fd0ee0a59c37ad
Kapusta45 <toha3306@gmail.com>
2025-02-26 10:32:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,55 +1,44 @@
 ﻿using System;
 
-public class HexTable
+class Program
 {
-    public static void Main(string[] args)
+    static void Main()
     {
-        int tableSize = 16; // Размер таблицы (до F в шестнадцатеричной системе)
+        // Размер таблицы
+        int size = 16;
 
-        // Печать таблицы умножения
-        Console.WriteLine("Таблица умножения (шестнадцатеричная)");
-        Console.Write("   "); // Отступ для заголовка
-        for (int i = 0; i < tableSize; i++)
+        // Печать заголовка таблицы
+        Console.Write("   |");
+        for (int i = 0; i < size; i++)
         {
-            Console.Write("{0,3} ", i.ToString("X")); // Заголовок столбцов (шестнадцатеричные числа)
+            Console.Write($" {i:X2} ");
         }
-        Console.WriteLine();
-        Console.WriteLine("---" + new string('-', tableSize * 4)); // Разделитель
-
-        for (int i = 0; i < tableSize; i++)
-        {
-            Console.Write("{0,2}| ", i.ToString("X")); // Заголовок строк (шестнадцатеричные числа)
-
-            for (int j = 0; j < tableSize; j++)
-            {
-                Console.Write("{0,3} ", (i * j).ToString("X")); // Результат умножения в шестнадцатеричной системе
-            }
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
+        Console.WriteLine("\n" + new string('-', 4 + size * 4));
 
         // Печать таблицы сложения
-        Console.WriteLine("Таблица сложения (шестнадцатеричная)");
-        Console.Write("   "); // Отступ для заголовка
-        for (int i = 0; i < tableSize; i++)
+        Console.WriteLine("Таблица сложения:");
+        for (int i = 0; i < size; i++)
         {
-            Console.Write("{0,3} ", i.ToString("X")); // Заголовок столбцов (шестнадцатеричные числа)
-        }
-        Console.WriteLine();
-        Console.WriteLine("---" + new string('-', tableSize * 4)); // Разделитель
-
-        for (int i = 0; i < tableSize; i++)
-        {
-            Console.Write("{0,2}| ", i.ToString("X")); // Заголовок строк (шестнадцатеричные числа)
-
-            for (int j = 0; j < tableSize; j++)
+            Console.Write($"{i:X2} |");
+            for (int j = 0; j < size; j++)
             {
-                Console.Write("{0,3} ", (i + j).ToString("X")); // Результат сложения в шестнадцатеричной системе
+                Console.Write($" {(i + j):X2} ");
             }
             Console.WriteLine();
         }
 
-        Console.ReadKey(); 
+        Console.WriteLine("\n" + new string('-', 4 + size * 4));
+
+        // Печать таблицы умножения
+        Console.WriteLine("Таблица умножения:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"{i:X2} |");
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($" {(i * j):X2} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
